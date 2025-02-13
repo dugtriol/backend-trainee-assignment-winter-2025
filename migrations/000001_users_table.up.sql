290dd0040de87933b13b28cc6856057afb05a973
aimandzhi <aigulmndzh@gmail.com>
2025-02-13 14:20:22 +0300

    change the size of fields

--- migrations/000001_users_table.up.sql
+++ migrations/000001_users_table.up.sql
@@ -4,8 +4,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users
 (
     id       UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(50)         NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255)         NOT NULL,
     amount   INT              DEFAULT 1000
 );
 
