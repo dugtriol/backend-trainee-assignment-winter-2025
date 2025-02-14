3b63d3f409e3305b162c6c721afd85ab9dce5c54
aimandzhi <aigulmndzh@gmail.com>
2025-02-14 12:54:43 +0300

    add token errors

--- internal/handler/http/v1/error.go
+++ internal/handler/http/v1/error.go
@@ -1,5 +1,7 @@
 package v1
 
+import "fmt"
+
 var (
 	MsgInvalidReq         = "Invalid request"
 	MsgFailedParsing      = "Failed to parse data"
@@ -10,4 +12,7 @@ var (
 
 	MsgForbidden         = "Forbidden"
 	MsgUserAlreadyExists = "User already exists"
+
+	ErrInvalidToken     = fmt.Errorf("invalid token")
+	ErrCannotParseToken = fmt.Errorf("cannot parse token")
 )
