1a6e09eb90542cc19d6458021abf3e51a56f44d4
aimandzhi <aigulmndzh@gmail.com>
2025-02-15 10:24:05 +0300

    add user errors

--- internal/handler/http/v1/error.go
+++ internal/handler/http/v1/error.go
@@ -8,11 +8,14 @@ var (
 	MsgInternalServerErr  = "Internal server error"
 	MsgInvalidPasswordErr = "Invalid password"
 
-	MsgUserNotFound = "User not found"
+	MsgUserNotFound      = "User not found"
+	MsgUserNotAuthorized = "User not authorized"
 
 	MsgForbidden         = "Forbidden"
 	MsgUserAlreadyExists = "User already exists"
 
 	ErrInvalidToken     = fmt.Errorf("invalid token")
 	ErrCannotParseToken = fmt.Errorf("cannot parse token")
+	ErrUserGet          = fmt.Errorf("user not get from database")
+	ErrNoUserInContext  = fmt.Errorf("no user in the context")
 )
