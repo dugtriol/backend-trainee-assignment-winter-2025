4f4105ef4e4e808ba8a7f92bda803f801db93fcb
aimandzhi <aigulmndzh@gmail.com>
2025-02-15 10:20:17 +0300

    edit inventory table

--- migrations/000003_inventories_table.up.sql
+++ migrations/000003_inventories_table.up.sql
@@ -5,8 +5,10 @@ CREATE TABLE IF NOT EXISTS inventories
 (
     id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     customer_id UUID REFERENCES users (id) ON DELETE CASCADE,
-    merch_id    INT REFERENCES merch (id) ON DELETE CASCADE,
+    type  VARCHAR(255) REFERENCES merch (name) ON DELETE CASCADE,
     quantity    INT              DEFAULT 1
 );
+
+ALTER TABLE inventories ADD CONSTRAINT unique_customer_merch UNIQUE (customer_id, type);
 
 COMMIT;
